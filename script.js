3b0398b336d15ead59e1943b4c1abde5ec6fe6d0
Praneeth <praneethayla@gmail.com>
2023-11-17 22:55:10 +0530

    used factory functions and added player and gameController module

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function gameBoard() {
+//  game board module
+const gameBoard = (() => {
 	const board = ["", "", "", "", "", "", "", "", ""];
 
-	const checkMark = (place) => {
-		if (board[place] != "") {
-			return true;
-		} else {
-			return false;
-		}
-	};
+	// check mark empty
+	const checkMark = (place) => board[place] === "";
 
+	// place mark
 	const placeMark = (mark, place) => {
-		if (checkMark(place) == true) {
-			console.log("try different place");
-		} else {
+		if (checkMark(place) === true) {
 			board[place] = mark;
+		} else {
+			console.log("try different place");
 		}
 	};
 
+	// check win
 	const checkWinner = () => {
 		if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
 			return true;
@@ -72,12 +70,78 @@ function gameBoard() {
 		placeMark,
 		checkWinner,
 	};
-}
+})();
 
-const n = gameBoard();
+// player module
+const playerModule = (() => {
+	const createPlayer = (mark) => {
+		return { mark };
+	};
 
-n.placeMark("o", 0);
-n.placeMark("o", 4);
-n.placeMark("o", 8);
+	return { createPlayer };
+})();
 
-console.log(n.checkWinner());
+// game logic
+
+const gameController = (() => {
+	const player1 = playerModule.createPlayer("X");
+	const player2 = playerModule.createPlayer("O");
+	let currentPlayer = player1;
+
+	// switched player
+	const switchPlayer = () => {
+		if (currentPlayer === player1) {
+			currentPlayer = player2;
+			console.log(`player2's turn ${player2.mark}`);
+		} else {
+			currentPlayer = player1;
+			console.log(`player1's turn ${player1.mark}`);
+		}
+	};
+
+	// play turn
+	const playTurn = (place) => {
+		if (gameBoard.checkMark(place)) {
+			gameBoard.placeMark(currentPlayer.mark, place);
+			gameController.checkStatus();
+			switchPlayer();
+			printBoard();
+		} else {
+			console.log("try place");
+		}
+	};
+
+	// console only test
+	const printBoard = () => {
+		console.log("Game Board:");
+		for (let i = 0; i < 3; i++) {
+			let row = "";
+			for (let j = 0; j < 3; j++) {
+				const index = i * 3 + j;
+				row += ` ${gameBoard.board[index] || "-"} `;
+				if (j < 2) {
+					row += "|";
+				}
+			}
+			console.log(row);
+			if (i < 2) {
+				console.log("-----------");
+			}
+		}
+		console.log("\n");
+	};
+
+	// check game status
+	const checkStatus = () => {
+		if (gameBoard.checkWinner()) {
+			console.log("won");
+		}
+	};
+
+	return { playTurn, checkStatus, currentPlayer, printBoard };
+})();
+
+// gameController.playTurn(0);
+// gameController.playTurn(4);
+// gameController.playTurn(4);
+// gameController.playTurn(8);
