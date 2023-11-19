c92b5b77894f197f142b20c4621e6202e9b3e0ce
Praneeth <praneethayla@gmail.com>
2023-11-19 21:33:57 +0530

    added gui

--- script.js
+++ script.js
@@ -9,59 +9,57 @@ const gameBoard = (() => {
 	const placeMark = (mark, place) => {
 		if (checkMark(place) === true) {
 			board[place] = mark;
-		} else {
-			console.log("try different place");
 		}
 	};
 
 	// check win
 	const checkWinner = () => {
 		if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
-			return true;
+			return { condition: true, mark: board[0] };
 		} else if (
 			board[3] !== "" &&
 			board[3] === board[4] &&
 			board[4] === board[5]
 		) {
-			return true;
+			return { condition: true, mark: board[3] };
 		} else if (
 			board[6] !== "" &&
 			board[6] === board[7] &&
 			board[7] === board[8]
 		) {
-			return true;
+			return { condition: true, mark: board[6] };
 		} else if (
 			board[0] !== "" &&
 			board[0] === board[3] &&
 			board[3] === board[6]
 		) {
-			return true;
+			return { condition: true, mark: board[0] };
 		} else if (
 			board[1] !== "" &&
 			board[1] === board[4] &&
 			board[4] === board[7]
 		) {
-			return true;
+			return { condition: true, mark: board[1] };
 		} else if (
 			board[2] !== "" &&
 			board[2] === board[5] &&
 			board[5] === board[8]
 		) {
-			return true;
+			return { condition: true, mark: board[2] };
 		} else if (
 			board[0] !== "" &&
 			board[0] === board[4] &&
 			board[4] === board[8]
 		) {
-			return true;
+			return { condition: true, mark: board[0] };
 		} else if (
 			board[2] !== "" &&
 			board[2] === board[4] &&
 			board[4] === board[6]
 		) {
-			return true;
+			return { condition: true, mark: board[2] };
 		} else {
-			return false;
+			return { condition: false };
 		}
 	};
 	return {
@@ -82,7 +80,6 @@ const playerModule = (() => {
 })();
 
 // game logic
-
 const gameController = (() => {
 	const player1 = playerModule.createPlayer("X");
 	const player2 = playerModule.createPlayer("O");
@@ -90,11 +87,18 @@ const gameController = (() => {
 
 	// switched player
 	const switchPlayer = () => {
+		gameController.checkStatus();
+
 		if (currentPlayer === player1) {
 			currentPlayer = player2;
+			playerDiv.textContent = `${currentPlayer.mark} chance`;
+
 			console.log(`player2's turn ${player2.mark}`);
+			return currentPlayer;
 		} else {
 			currentPlayer = player1;
+			playerDiv.textContent = `${currentPlayer.mark} chance`;
+
 			console.log(`player1's turn ${player1.mark}`);
 		}
 	};
@@ -102,12 +106,16 @@ const gameController = (() => {
 	// play turn
 	const playTurn = (place) => {
 		if (gameBoard.checkMark(place)) {
+			errorDiv.textContent = "";
+
 			gameBoard.placeMark(currentPlayer.mark, place);
 			gameController.checkStatus();
 			switchPlayer();
 			printBoard();
 		} else {
-			console.log("try place");
+			errorDiv.textContent =
+				"Cell already occupied. Try a different place.";
+			console.log("Cell already occupied. Try a different place.");
 		}
 	};
 
@@ -133,15 +141,22 @@ const gameController = (() => {
 
 	// check game status
 	const checkStatus = () => {
-		if (gameBoard.checkWinner()) {
-			console.log("won");
+		const winner = gameBoard.checkWinner();
+		if (winner.condition) {
+			console.log("Player", winner.mark, "won!");
 		}
 	};
-
 	return { playTurn, checkStatus, currentPlayer, printBoard };
 })();
 
-// gameController.playTurn(0);
-// gameController.playTurn(4);
-// gameController.playTurn(4);
-// gameController.playTurn(8);
+const optionsBtn = document.querySelectorAll(".cell");
+optionsBtn.forEach((button) =>
+	button.addEventListener("click", () => {
+		place = button.id;
+		gameController.playTurn(place);
+		button.textContent = gameBoard.board[place];
+	})
+);
+
+const playerDiv = document.querySelector(".player");
+const errorDiv = document.querySelector("#errorCell");
